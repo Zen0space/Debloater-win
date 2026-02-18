94fa74cc0d77c96f979c72041d5c37a547f9b17f
Zen0space <tartnenas_kai@outlook.com>
2026-02-19 01:42:31 +0800

    fix: update build configuration

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,8 @@
 fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        println!("cargo:rustc-link-arg=/MANIFEST:EMBED");
+        println!("cargo:rustc-link-arg=/MANIFESTINPUT:app.manifest");
+    }
     tauri_build::build()
 }
